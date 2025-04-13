da9ddbbdbb0f378574029893ddc340fc6812c6df
Zuhair Arif <zuhairarif1234@gmail.com>
2025-04-14 05:14:54 +0530

    final touches

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,64 @@
-import { AppSidebar } from "@/components/app-sidebar"
+import { getUser } from "@/lib/user";
+import Uploader from "@/components/forms/uploader";
+import type { Document, UserType } from "@/types/user";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { UploadSheet } from "./upload-sheet";
+import { getAllUploadedDocuments } from "@/lib/upload";
+import UploaderForm from "@/components/forms/uploader";
+import { File } from "lucide-react";
+import { DocsList } from "@/components/doc-list";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
-import Link from "next/link"
-import type { PropsWithChildren } from "react"
-export default async function Page(props: PropsWithChildren<{params: Record<string, string>}>) {
-  console.log({props})
+	groupDocumentsByDate,
+	sortGroupedDocumentsByDate,
+} from "@/lib/document-utils";
 
-  console.log('keys: ', Object.keys( props.params))
-  return (
 
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <Link href="/dashboard/daily-upload">Upload Files</Link>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-        <div className="bg-muted/50 aspect-video rounded-xl" />
-      </div>
-      <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
-    </div>
 
-  )
+export default async function Page() {
+	const user = (await getUser()) as UserType & { id: string };
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	const docs = (await getAllUploadedDocuments(user.id)) as any & {
+		toJSON: () => void;
+	};
+
+	const groupedDocs = groupDocumentsByDate(docs as Document[]);
+	const sortedGroupedDocs = sortGroupedDocumentsByDate(groupedDocs);
+
+	console.log({ sortedGroupedDocs: sortedGroupedDocs["4/5/2025"][0] });
+
+	return (
+		<div className="flex flex-1 flex-col gap-4 p-4 pt-0 w-full">
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+				Documents
+			</h1>
+			<p className="leading-7 [&:not(:first-child)]:mt-6">
+				All your uploaded documents with dates
+			</p>
+			{/* <div className="flex max-w-lg items-center space-x-2">
+				<Input
+					type="search"
+					placeholder="Search Documents"
+					className="w-full"
+				/>
+				
+			</div> */}
+			<UploadSheet
+				trigger={
+					<Button type="submit" className="max-w-sm">
+						Upload{" "}
+					</Button>
+				}
+			>
+				<UploaderForm
+					subjects={user.subjects.map((i) => ({
+						subject: i.subject,
+						code: i.serial,
+					}))}
+					userId={user.id}
+				/>
+			</UploadSheet>
+			<DocsList groupedDocs={sortedGroupedDocs} />
+		</div>
+	);
 }
