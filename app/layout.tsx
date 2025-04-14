102d42a7f1262bd63f15e52fbdeb3d23632b171c
Zuhair Arif <zuhairarif1234@gmail.com>
2025-04-14 07:04:36 +0530

    uploaded facivon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<link rel="shortcut icon" href="favicon.png" type="image/png" />
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
